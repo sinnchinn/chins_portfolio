3cab259f6a02fe02a581fb62013bb6cb774da490
sinnchinn <sinathachin@gmail.com>
2024-05-06 00:27:08 -0700

    fix

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -29,7 +29,7 @@ const ProjectsPage: React.FC = () => {
 
   // Sample items array
   const items = [
-    { title: 'MangaDiction!', content: 'Mangadiction is a Fullstack application that was assigned in the last level of CodeStack before the internship. I am working in a group of 3 people, where I am one of the Front End developers. MangaDiction is an app mainly for manga readers to connect with one another. This application is still a work in progress but I am excited to see how far we can get.', src: '/md.png', icons: ['/react.png', 'flowbite.png', '/material.png', '/ts.png', '/tw.png', '/c.png'], button: 'MangaDiction!', href: 'https://manga-diction.vercel.app/' },
+    { title: 'MangaDiction!', content: 'Mangadiction is a Fullstack application that was assigned in the last level of CodeStack before the internship. I am working in a group of 3 people, where I am one of the Front End developers. MangaDiction is an app mainly for manga readers to connect with one another. This application is still a work in progress but I am excited to see how far we can get.', src: '/md.png', icons: ['/react.png', '/flowbite.png', '/material.png', '/ts.png', '/tw.png', '/c.png'], button: 'MangaDiction!', href: 'https://manga-diction.vercel.app/' },
     { title: 'TeaAmo', content: 'TeaAmo is a business redevelopment website for a small business named TeaAmo. Me and a team of 2 others created this site to make it easy to navigate and better sell their products.', src: '/bd.png', icons: ['/js.png', '/bs.png', '/html.png'], button: 'TeaAmo', href: 'https://business-redevelop.vercel.app/' },
     { title: 'PokeGo', content: 'PokeGo is a Next JS app that I created to be able to search up any pokemons by name and id number. I coded this site to where you can only search generations 1-5 pokemons. You are able to randomize and favorite pokemons of your liking.', src: '/pr.png', icons: ['/react.png', '/ts.png', '/flowbite.png', '/material.png'], button: 'PokeGo', href: 'https://chins-pokereact.vercel.app/' },
   ];
@@ -75,27 +75,27 @@ const ProjectsPage: React.FC = () => {
             <div className='col-span-1'>
               <p className='text-xl'> Languages: </p>
               <div className='inline-flex pt-1'>
-                <Image alt='' className='w-10 h-8 mr-1' src='/html.png' />
-                <Image alt='' className='w-9 h-8 mr-1' src='/js.png' />
-                <Image alt='' className='w-8 h-8 mr-1' src='/ts.png' />
-                <Image alt='' className='w-8 h-8' src='/c.png' />
+                <Image height={100} width={100}  alt='' className='w-10 h-8 mr-1' src='/html.png' />
+                <Image height={100} width={100}  alt='' className='w-9 h-8 mr-1' src='/js.png' />
+                <Image height={100} width={100}  alt='' className='w-8 h-8 mr-1' src='/ts.png' />
+                <Image height={100} width={100}  alt='' className='w-8 h-8' src='/c.png' />
               </div>
             </div>
 
             <div className='col-span-'>
               <p className='text-xl'>Libraries: </p>
               <div className='pt-1 inline-flex'>
-                <Image alt=''  className='w-10 h-8 mr-1' src='/react.png' />
-                <Image alt=''  className='w-10 h-8 mr-1' src='/flowbite.png' />
-                <Image alt=''  className='w-10 h-8' src='/material.png' />
+                <Image height={100} width={100} alt=''  className='w-10 h-8 mr-1' src='/react.png' />
+                <Image height={100} width={100} alt=''  className='w-10 h-8 mr-1' src='/flowbite.png' />
+                <Image height={100} width={100} alt=''  className='w-10 h-8' src='/material.png' />
               </div>
             </div>
 
             <div className='col-span-1'>
               <p className='text-xl'>Frameworks: </p>
               <div className='inline-flex pt-1'>
-                <Image  alt='' className='w-8 h-8 mr-1' src='/bs.png' />
-                <Image alt=''  className='w-11 h-7' src='/tw.png' />
+                <Image height={100} width={100} alt='' className='w-8 h-8 mr-1' src='/bs.png' />
+                <Image height={100} width={100} alt=''  className='w-11 h-7' src='/tw.png' />
 
               </div>
             </div>
@@ -125,7 +125,7 @@ const ProjectsPage: React.FC = () => {
               whileHover={{ scale: 1.1 }}
               whileTap={{ scale: 0.9 }}
             >
-              <Image  src={item.src} alt={`Image ${item.src}`} className="w-full h-auto mb-2 rounded-lg" />
+              <Image height={600} width={600}  src={item.src} alt={`Image ${item.src}`} className="w-full h-auto mb-2 rounded-lg" />
               <p className="text-3xl font-semibold text-center">{item.title}</p>
             </motion.div>
           ))}
@@ -146,13 +146,13 @@ const ProjectsPage: React.FC = () => {
                   animate={{ scale: 1 }}
                   exit={{ scale: 0 }}
                 >
-                  <Image  src={selectedItem.src} alt={`Image ${selectedItem.src}`} className="w-full object-top h-96 object-cover mb-2 rounded-lg" />
+                  <Image height={1000} width={1000}  src={selectedItem.src} alt={`Image ${selectedItem.src}`} className="w-full object-top h-96 object-cover mb-2 rounded-lg" />
                   <p className="text-lg p-5">{selectedItem.content}</p>
                   <div className='px-5 grid grid-cols-2'>
                     <div>
                       <p className='pb-1 text-lg font-semibold'>Built With:</p>
                       {selectedItem.icons.map((icon, iconIndex) => (
-                        <Image key={iconIndex} src={icon} alt={`Icon ${iconIndex}`} className="w-10 inline-flex h-10 mr-5" />
+                        <Image height={100} width={100}  key={iconIndex} src={icon} alt={`Icon ${iconIndex}`} className="w-10 inline-flex h-10 mr-5" />
                       ))}
                     </div>
                     <div className='flex justify-center'>
@@ -213,7 +213,7 @@ const ProjectsPage: React.FC = () => {
               {isResumeOpen && (
                 <div className="fixed top-0 left-0 w-full h-full flex justify-center items-center bg-gray-900 bg-opacity-50">
                   <div className=" w-2/6 bg-white border-navPink border-8 p-8 rounded-lg shadow-lg">
-                    <Image src={'/resume.png'} alt="Resume" className="w-6/6 object-top object-cover mb-2 rounded-lg " />
+                    <Image height={800} width={800} src={'/resume.png'} alt="Resume" className="w-6/6 object-top object-cover mb-2 rounded-lg " />
                     {/* Add any additional content for the resume card */}
                     <button className="px-5 mt-5 hover:underline" onClick={closeResume}>Close</button>
                   </div>
